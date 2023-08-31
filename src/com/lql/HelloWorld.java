87f92b9492d8eb89d1cbd367692ab322f12c1163
liqiuliang <liqiuliang@bilibili.com>
2023-08-31 19:49:37 +0800

    bugFix 2

--- src/com/lql/HelloWorld.java
+++ src/com/lql/HelloWorld.java
@@ -9,5 +9,6 @@ public class HelloWorld {
         System.out.println("master 1");
         System.out.println("master 2");
         System.out.println("bugFix 1");
+        System.out.println("bugFix 2");
     }
 }
