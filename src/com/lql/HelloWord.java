1daa233057b2626af44673bc355886b084e5295f
liqiuliang <liqiuliang@bilibili.com>
2023-09-05 19:33:00 +0800

    test

--- src/com/lql/HelloWord.java
+++ src/com/lql/HelloWord.java
@@ -7,5 +7,7 @@ package com.lql;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("修改");
+        System.out.println("修改2");
     }
 }
