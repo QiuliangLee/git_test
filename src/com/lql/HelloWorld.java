d7c736a33f15fa3dd049dac7cfeb19e42d004b1a
liqiuliang <liqiuliang@bilibili.com>
2023-09-05 20:02:41 +0800

    1

---
+++ src/com/lql/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lql;
+
+/**
+ * @author liqiuliang
+ * @create 2023-09-05 20:02
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
