7031dcd8e19089a1ad08a00350078d1766872842
liqiuliang <liqiuliang@bilibili.com>
2023-09-05 20:01:51 +0800

    1

--- src/com/lql/HelloWord.java
+++ src/com/lql/HelloWord.java
@@ -6,6 +6,6 @@ package com.lql;
  */
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("4452");
     }
 }
